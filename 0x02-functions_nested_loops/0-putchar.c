5aa36f1887a3fb0e4ab9d976c8f18371ae252aa6
woldie <woldieb77@gmal.com>
2023-07-18 05:30:52 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,11 @@
-#include "main.h";
+#include "main.h"
 
-/** main print _putchar
- * Returen : 0
+/**
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -17,5 +16,5 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 
-return : 0;
+	return (0);
 }
