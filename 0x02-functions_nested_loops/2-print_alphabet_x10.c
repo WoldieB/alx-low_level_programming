b1457493772674b81b9def39182be9a14e5a5b4c
woldie <woldieb77@gmal.com>
2023-07-18 00:39:35 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 in lower case
+ */
+
+void print_alphabet_x10(void)
+{
+	int i=0;
+	char ch='a';
+	
+	while(i<10)
+	{
+		while(ch<='z')
+		{
+			_putchar(ch);
+			ch++;
+	
+		}
+
+		i++;
+
+	}
+
+}
+		
+
