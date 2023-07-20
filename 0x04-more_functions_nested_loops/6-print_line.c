25eb7926e25cace47be3e99cfe478d7d48bfc164
woldie <woldieb77@gmal.com>
2023-07-20 10:08:39 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,9 +1,10 @@
 #include "main.h"
+
 /**
- * *print_line - prints a straight line
- * *@n:parameter
- * *Return:returns nothing
- * */
+ * print_line - prints a straight line
+ * @n:parameter
+ * Return:returns nothing
+ */
 void print_line(int n)
 {
 	while (n-- > 0)
