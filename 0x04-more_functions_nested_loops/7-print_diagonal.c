b767fa44f9d20fbc345407b47c6c5848f45fb46e
woldie <woldieb77@gmal.com>
2023-07-20 10:13:08 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,12 +1,14 @@
 #include "main.h"
+
 /**
- * *print_diagonal - prints a diagonal
- * *@n:parameter
- * *Return:returns nothing
- * */
+ * print_diagonal - prints a diagonal
+ * @n:parameter
+ * Return:returns nothing
+ */
 void print_diagonal(int n)
 {
 	int len, space;
+
 	if (n > 0)
 	{
 		for (len = 0; len < n; len++)
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * *print_square - prints squares
- * *@size: parameter
- * *Return:returns nothing
- * */
+ * print_square - prints squares
+ * @size: parameter
+ * Return:returns nothing
+ */
 void print_square(int size)
 {
 	int inc1, inc2;
+
 	if (size > 0)
 	{
 		for (inc1 = 0; inc1 < size; inc1++)
