2393859dd42df86782fe2e850d01a82455866e33
woldie <woldieb77@gmal.com>
2023-07-27 06:52:20 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,32 +1,24 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _strcat - concatenates the string pointed to by @src to
- * the end of the string pointed to by @dest
- *@dest: String that will append
- *@src: string that will be concatenated upon
+ *_strcat - Write a function that concatenates two strings.
  *
- * Return: returns pointer to @dest
+ *@dest: This is the output dest
+ *@src: This is the input source
+ *
+ * Return: This return to dest, that concatenates two strings
  */
 
-/*
-1. First, we’re declaring a function called _strcat that takes two arguments: dest and src.
-2.  Next, we’re declaring two variables: index and dest_len.
-3.  Then, we’re using a while loop to iterate through the dest string and find its length.
-4.  Next, we’re using a for loop to iterate through the src string and append each character to the end of the dest string.
-5.  Finally, we’re returning the dest string.
-*/
-/*
-Time Complexity: O(n)
-*/
 char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	int i, j;
 
-	while (dest[index++])
-		dest_len++;
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
 	return (dest);
 }
