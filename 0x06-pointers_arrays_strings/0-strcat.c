27c7f5477c455d9b547b1ec19162f469e3541b3e
woldie <woldieb77@gmal.com>
2023-07-27 03:41:42 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,10 @@
 #include "main.h"
 
 /**
- *
+  * _strcat - concatenates 2 strings.
+ * @dest: string with concatenation
+ * @src: string to be concatenated
+ * Return: Always 0.
  */
 
 char *_strcat(char *dest, char *src)
