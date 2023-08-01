77371f0222395e05262079166b1a517b5fec373e
woldie <woldieb77@gmal.com>
2023-07-31 23:03:35 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,21 +1,19 @@
-nclude "main.h"
-
+#include "main.h"
 /**
- *  print_chessboard - print a chessboard.
- *    @a: source matrix
- *     Return: chessboard
+ * print_chessboard - function that prints the chessboard.
+ *@a: first value -char
+ *
  */
-
 void print_chessboard(char (*a)[8])
 {
-		int i, j;
+	int i, j;
 
-			for (i = 0; i <= 7; i++)
-					{
-								for (j = 0; j <= 7; j++)
-											{
-															_putchar(a[i][j]);
-																	}
-										_putchar('\n');
-											}
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar (a[i][j]);
+		}
+		_putchar('\n');
+	}
 }
