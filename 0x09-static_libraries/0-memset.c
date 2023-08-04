fcf78b06a822ce1aa449e85c5f54e5f92c3e5487
woldie <woldieb77@gmal.com>
2023-08-04 03:12:27 -0700

    editor

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -7,14 +7,15 @@
  *      @n: lenght of buffer
  *       Return: new string.
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
-                unsigned int i = 0;
+	unsigned int i = 0;
 
-                        while (i < n)
-                        {
-                                *(s + i) = b;
-                                i++;
-                        }                                                       }
-                                return (s);
+	while (i < n)
+	{
+		*(s + i) = b;
+		i++;
+	}
+	return (s);
 }
