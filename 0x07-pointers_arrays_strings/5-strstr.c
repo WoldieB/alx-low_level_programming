e1a01256f8b06ad6841f0a1450ce00aeda426e9d
woldie <woldieb77@gmal.com>
2023-08-04 01:54:43 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,19 +8,20 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-		while (*haystack)
-				{
-							if ((*haystack == *needle && coincidence(haystack, needle) == 1) || !*needle)
-										{
-														return (haystack);
-																}
-									else
-												{
-																haystack++;
-																		}
-										}
-			return (0);
+	while (*haystack)
+	{
+		if ((*haystack == *needle && coincidence(haystack, needle) == 1) || !*needle)
+		{
+			return (haystack);
+		}
+		else
+		{
+			haystack++;
+		}
+	}
+	return (0);
 }
+
 /**
  *   coincidence - define if the string b is inside a.
  *    @a: source string
@@ -29,14 +30,13 @@ char *_strstr(char *haystack, char *needle)
  */
 int coincidence(char *a, char *b)
 {
-		while (*b && *b == *a)
-				{
-							b++;
-									a++;
-										}
-
-			if (*b == '\0')
-						return (1);
-				else
-							return (0);
+	while (*b && *b == *a)
+	{
+		b++;
+		a++;
+	}
+	if (*b == '\0')
+		return (1);
+	else
+		return (0);
 }
