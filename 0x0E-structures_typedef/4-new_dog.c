63eaca4a248e5b1f3a029b1d1bb2a84a757969de
woldie <woldieb77@gmal.com>
2023-08-14 22:54:36 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,20 +15,21 @@ int _strlen(char *str)
 	return (len);
 }
 
+
 /**
- * _strcopy - copy string pointed by src into dest variable
+ * _strcopy - copy string pointed by src
+ * into dest variable
  * @dest:buffer storing string copy
  * @src: buffer storing string to copy
  * Return:returns copied string
 */
-
 char *_strcopy(char *dest, char *src)
 {
-	int i = 0;
+	int index = 0;
 
-	for (; src[i] ; i++)
-		dest[i] = src[i];
-	dest[i] = '\0';
+	for (; src[index] ; index++)
+		dest[index] = src[index];
+	dest[index] = '\0';
 	return (dest);
 }
 
@@ -39,33 +40,30 @@ char *_strcopy(char *dest, char *src)
  * @owner: owner of new dog
  * Return: returns NULL in case of failure
 */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog1;
+	dog_t *doggo;
 
 	if (name == NULL || age < 0 || owner == NULL)
 		return (NULL);
-	dog1 = malloc(sizeof(dog_t));
-	if (dog1 == NULL)
+	doggo = malloc(sizeof(dog_t));
+	if (doggo == NULL)
 		return (NULL);
-	dog1->name = malloc(sizeof(char) * (_strlen(name) + 1));
-	if (dog1->name == NULL)
+	doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (doggo->name == NULL)
 	{
-		free(dog1);
+		free(doggo);
 		return (NULL);
 	}
-
-	dog1->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
-	if (dog1->owner == NULL)
-	{
-		free(dog1->name);
-		free(dog1);
-		return (NULL);
-	}
-
-	dog1->name = _strcopy(dog1->name, name);
-	dog1->age = age;
-	dog1->owner = _strcopy(dog1->owner, owner);
-	return (dog1);
+	doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1))
+		if (doggo->owner == NULL)
+		{
+			free(doggo->name);
+			free(doggo);
+			return (NULL);
+		}
+	doggo->name = _strcopy(doggo->name, name);
+	doggo->age = age;
+	doggo->owner = _strcopy(doggo->owner, owner);
+	return (doggo);
 }
