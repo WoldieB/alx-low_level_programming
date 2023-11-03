0f19076340ba73c0965b42633b6e74235cd98f96
woldieb $ balcha be <woldie Birhane>
2023-11-03 07:02:19 -0700

    tasks

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,19 +8,25 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int i;
-	hash_node_t *tmp;
+        unsigned long int k_index;
+        hash_node_t *tmp;
 
-	if (!ht || !key)
-		return (NULL);
-	i = key_index((unsigned char *)key, ht->size);
-	tmp = ht->array[i];
-	if (tmp)
-	{
-		while (strcmp(tmp->key, key) != 0)
-			tmp = tmp->next;
-		if (tmp)
-			return (tmp->value);
-	}
-	return (NULL);
+        if (ht == NULL)
+                return (NULL);
+
+        if (key == NULL || *key == '\0')
+                return (NULL);
+
+        k_index = key_index((unsigned char *)key, ht->size);
+
+        tmp = ht->array[k_index];
+
+        while (tmp != NULL)
+        {
+                if (strcmp(tmp->key, key) == 0)
+                        return (tmp->value);
+                tmp = tmp->next;
+        }
+
+        return (NULL);
 }
